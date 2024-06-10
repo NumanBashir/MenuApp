14a171f0365976b4c45d09c16b27b18bf0d93d8a
NumanBashir <sportybashir@gmail.com>
2024-06-10 12:45:30 +0200

    context done, create database

--- MenuApp/Data/MenuContext.cs
+++ MenuApp/Data/MenuContext.cs
@@ -32,10 +32,93 @@ namespace MenuApp.Data
             // The Ingredient entity has a collection of DishIngredient entities.
             // The DishIngredient entity has a reference to an Ingredient entity.
             // The relationship is one-to-many
-            modelBuilder.Entity<DishIngredient  >()
+            modelBuilder.Entity<DishIngredient>()
                 .HasOne(i => i.Ingredient)
                 .WithMany(di => di.DishIngredients)
                 .HasForeignKey(i => i.IngredientId);
+
+            modelBuilder.Entity<Dish>().HasData(
+                new Dish
+                {
+                    Id = 1,
+                    Name = "Salatpizza",
+                    ImageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmandekogebogen.dk%2Fsalatpizza.1114.html&psig=AOvVaw1oDK58q7KvBsVpSKZrXP32&ust=1718097000159000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiZ853Y0IYDFQAAAAAdAAAAABAE",
+                    Price = 10.99
+                },
+                new Dish
+                {
+                    Id = 2,
+                    Name = "Seafood Boil",
+                    ImageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.allrecipes.com%2Frecipe%2F279460%2Fold-bay-seafood-boil%2F&psig=AOvVaw0l5bQBmH_gqs_lfPQ0qZVM&ust=1718097032272000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOC_1qzY0IYDFQAAAAAdAAAAABAw",
+                    Price = 20.99
+                }
+            );
+
+            modelBuilder.Entity<Ingredient>().HasData(
+                new Ingredient
+                {
+                    Id = 1,
+                    Name = "Cheese"
+                },
+                new Ingredient
+                {
+                    Id = 2,
+                    Name = "Tomato Sauce"
+                },
+                new Ingredient
+                {
+                    Id = 3,
+                    Name = "Kebab"
+                },
+                new Ingredient
+                {
+                    Id = 4,
+                    Name = "Shrimp"
+                },
+                new Ingredient
+                {
+                    Id = 5,
+                    Name = "Crab"
+                },
+                new Ingredient
+                {
+                    Id = 6,
+                    Name = "Corn"
+                }
+            );
+
+            modelBuilder.Entity<DishIngredient>().HasData(
+                new DishIngredient
+                {
+                    DishId = 1,
+                    IngredientId = 1
+                },
+                new DishIngredient
+                {
+                    DishId = 1,
+                    IngredientId = 2
+                },
+                new DishIngredient
+                {
+                    DishId = 1,
+                    IngredientId = 3
+                },
+                new DishIngredient
+                {
+                    DishId = 2,
+                    IngredientId = 4
+                },
+                new DishIngredient
+                {
+                    DishId = 2,
+                    IngredientId = 5
+                },
+                new DishIngredient
+                {
+                    DishId = 2,
+                    IngredientId = 6
+                }
+            );
 
             base.OnModelCreating(modelBuilder);
         }
---
+++ MenuApp/Migrations/20240610092159_Initial migration.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MenuApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dishes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dishes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DishIngredients",
+                columns: table => new
+                {
+                    DishId = table.Column<int>(type: "int", nullable: false),
+                    IngredientId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DishIngredients", x => new { x.DishId, x.IngredientId });
+                    table.ForeignKey(
+                        name: "FK_DishIngredients_Dishes_DishId",
+                        column: x => x.DishId,
+                        principalTable: "Dishes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DishIngredients_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dishes",
+                columns: new[] { "Id", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmandekogebogen.dk%2Fsalatpizza.1114.html&psig=AOvVaw1oDK58q7KvBsVpSKZrXP32&ust=1718097000159000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiZ853Y0IYDFQAAAAAdAAAAABAE", "Salatpizza", 10.99 },
+                    { 2, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.allrecipes.com%2Frecipe%2F279460%2Fold-bay-seafood-boil%2F&psig=AOvVaw0l5bQBmH_gqs_lfPQ0qZVM&ust=1718097032272000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOC_1qzY0IYDFQAAAAAdAAAAABAw", "Seafood Boil", 20.989999999999998 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Ingredients",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Cheese" },
+                    { 2, "Tomato Sauce" },
+                    { 3, "Kebab" },
+                    { 4, "Shrimp" },
+                    { 5, "Crab" },
+                    { 6, "Corn" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "DishIngredients",
+                columns: new[] { "DishId", "IngredientId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 2 },
+                    { 1, 3 },
+                    { 2, 4 },
+                    { 2, 5 },
+                    { 2, 6 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DishIngredients_IngredientId",
+                table: "DishIngredients",
+                column: "IngredientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DishIngredients");
+
+            migrationBuilder.DropTable(
+                name: "Dishes");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+        }
+    }
+}
